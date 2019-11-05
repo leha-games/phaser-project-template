6da324c1f5aca046d405a27fdb3bafd1cd681091
Alexey Grigoriev <grialexey@gmail.com>
2019-11-05 14:13:30 +0300

    Add yandex.games integration

--- webpack/yandex.js
+++ webpack/yandex.js
@@ -3,19 +3,24 @@ const merge = require("webpack-merge");
 const path = require("path");
 const prod = require("./prod");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 
 module.exports = merge(prod, {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./index.html",
-            USE_CORDOVA: true
+            USE_YANDEX_GAMES_SDK: true
+        }),
+        new ServiceWorkerWebpackPlugin({
+            entry: path.join(__dirname, '../src/sw.js'),
+            publicPath: ''
         }),
         new webpack.DefinePlugin({
-            USE_CORDOVA: true
+            USE_YANDEX_GAMES_SDK: true
         })
     ],
     output: {
         filename: "bundle.min.js",
-        path: path.resolve(__dirname, '../cordova/www'),
+        path: path.resolve(__dirname, '../yandex_games'),
     },
 });
